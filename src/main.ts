6558ffc0e783e324325bb71050e1f34a8016db9b
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-18 00:10:04 +0200

    before install

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import './style.scss'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>('#app')!.innerHTML =/*html*/ `
 <main class="grid place-items-center">
-	<h1 class="text-green-800 text-7xl">This is a test of Tailwind</h1>
-	<p class="text-3xl mt-7 italic w-1/2 leading-10 forceLineHeight">Lorem ipsum, dolor sit <span class="highlight">amet</span> consectetur adipisicing elit. Ab consequatur, et natus quis at repellendus rerum quas! Atque fuga repudiandae laborum <span class="highlight">eveniet</span> facilis, harum quas dignissimos <span class="highlight">okisdje</span>? Deleniti, ipsam?</p>
+	<!-- <h1 class="text-green-800 text-7xl">This is a test of Tailwind</h1>
+	<p class="text-3xl mt-7 italic w-1/2 leading-10 forceLineHeight">Lorem ipsum, dolor sit <span class="highlight">amet</span> consectetur adipisicing elit. Ab consequatur, et natus quis at repellendus rerum quas! Atque fuga repudiandae laborum <span class="highlight">eveniet</span> facilis, harum quas dignissimos <span class="highlight">okisdje</span>? Deleniti, ipsam?</p> -->
+
+
+<div class="flex gap-6">
+	<button class="">create page</button>
+</div>
 </main>
 `
