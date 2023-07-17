a58b964085668cc765fad1ca51ec2cc40466c1b0
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-18 01:10:49 +0200

    interactive buttons

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import Separator from './components/Separator';
 document.querySelector<HTMLDivElement>('#app')!.innerHTML =/*html*/ `
 <div class="flex gap-6" x-data="{message: ''}">
 	<button class="btn btn-primary" @click="message = 'create the page'">Create Page</button>
+	<button class="btn btn-secondary" @click="message = 'get information'">Get Information</button>
 	<button class="btn btn-danger" @click="message = 'delete layout'">Delete Layout</button>
 	<div class="text-gray-500 text-3xl" x-text="message"></div>
 </div>
