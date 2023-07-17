49b23e7019ff73cc7c49a37249e72e190d7d5de4
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-17 22:53:58 +0200

    finished

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import './style.css'
+import './style.scss'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <main class="grid place-items-center">
 	<h1 class="text-green-800 text-7xl">This is a test of Tailwind</h1>
-	<p class="text-3xl mt-7 italic w-1/2 leading-10">Lorem ipsum, dolor sit <span class="highlight">amet</span> consectetur adipisicing elit. Ab consequatur, et natus quis at repellendus rerum quas! Atque fuga repudiandae laborum <span class="highlight">eveniet</span> facilis, harum quas dignissimos <span class="highlight">okisdje</span>? Deleniti, ipsam?</p>
+	<p class="text-3xl mt-7 italic w-1/2 leading-10 forceLineHeight">Lorem ipsum, dolor sit <span class="highlight">amet</span> consectetur adipisicing elit. Ab consequatur, et natus quis at repellendus rerum quas! Atque fuga repudiandae laborum <span class="highlight">eveniet</span> facilis, harum quas dignissimos <span class="highlight">okisdje</span>? Deleniti, ipsam?</p>
 </main>
 `
