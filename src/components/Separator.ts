519fa4d903b62b072ad2b26fba5d165a57dedfe0
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-18 07:37:07 +0200

    four sections

--- src/components/Separator.ts
+++ src/components/Separator.ts
@@ -1,4 +1,3 @@
-const Separator = () => {
+export const Separator = () => {
 	return '<div class="my-5 border-gray-800 border-b"></div>';
 }
-export default Separator;
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,30 @@
 import './style.scss';
-import Separator from './components/Separator';
 import { ItemHeader } from './components/ItemHeader';
+import { Separator } from './components/Separator';
 
 // this works
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML =/*html*/ `
+
 ${ItemHeader('Interactive Buttons')}
-<div class="flex gap-6" x-data="{message: ''}">
-	<button class="btn btn-primary" @click="message = 'create the page'">Create Page</button>
-	<button class="btn btn-secondary" @click="message = 'get information'">Get Information</button>
-	<button class="btn btn-danger" @click="message = 'delete layout'">Delete Layout</button>
-	<div class="text-gray-500 text-3xl" x-text="message"></div>
+<div class="flex gap-2" x-data="{message: ''}">
+	<button class="btn-primary" @click="message = 'copied'">Copy</button>
+	<button class="btn-primary" @click="message = 'sent'">Send</button>
+	<button class="btn-primary" @click="message = 'saved'">Save</button>
+	<button class="btn-danger" @click="message = 'deleted'">Delete</button>
+	<div class="text-gray-500 text-3xl" x-text="message"></div>		
 </div>
 ${Separator()}
 
+${ItemHeader('Employee Cards')}
+<div class="text-gray-400">(todo)</div>
+${Separator()}
+
+${ItemHeader('Feature 3')}
+<div class="text-gray-400">(todo)</div>
+${Separator()}
+
+${ItemHeader('Feature 4')}
+<div class="text-gray-400">(todo)</div>
+${Separator()}
 `
